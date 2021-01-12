526da148662a73b0ba2570be1c95e8b83312d803
rreiners <58253028+rreiners@users.noreply.github.com>
2021-01-12 21:24:24 +0100

    finished round 2 and added logic to database

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = '022fde4f6f0721b9ed817c5ae18edb54307600af64379f5120b5a1553f8bab52'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xjezuiuthzzkax:e66256fb0f46249a929c24c9ad581ff139a192ffa376536e152f948061afdc9f@ec2-54-216-202-161.eu-west-1.compute.amazonaws.com:5432/dbgg781qn34tnm' #sqlite:///users.sqlite3' #
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xjezuiuthzzkax:e66256fb0f46249a929c24c9ad581ff139a192ffa376536e152f948061afdc9f@ec2-54-216-202-161.eu-west-1.compute.amazonaws.com:5432/dbgg781qn34tnm'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db = SQLAlchemy(app)
 
@@ -21,26 +20,41 @@ class Data(db.Model):
     recieve_US = db.Column(db.Integer)
     recieve_TW = db.Column(db.Integer)
     recieve_CHN = db.Column(db.Integer)
+    analysis = db.Column(db.Integer)
+    measure1 = db.Column(db.Integer)
+    measure2 = db.Column(db.Integer)
+    measure3 = db.Column(db.Integer)
     inventory = db.Column(db.Integer)
     profit = db.Column(db.Integer)
 
-    def __init__(self, order_US, order_TW, order_CHN, recieve_US, recieve_TW, recieve_CHN, inventory, profit):
+    def __init__(self, order_US, order_TW, order_CHN, recieve_US, recieve_TW, recieve_CHN, analysis, measure1, measure2, measure3, inventory, profit):
         self.order_US = order_US
         self.order_TW = order_TW
         self.order_CHN = order_CHN
         self.recieve_US = recieve_US
         self.recieve_TW = recieve_TW
         self.recieve_CHN = recieve_CHN
+        self.analysis = analysis
+        self.measure1 = measure1
+        self.measure2 = measure2
+        self.measure3 = measure3
         self.inventory = inventory
         self.profit = profit
 
 @app.route('/')                
 def home():
-    
     return render_template("home.html", content = "testing")
 
 @app.route('/round1/', methods=['GET', 'POST'])
 def round1():
+
+    db.session.query(Data).delete()
+    db.session.commit()
+
+    for i in range(1,10):
+        z = Data(0,0,0,0,0,0,0,0,0,0,0,0)
+        db.session.add(z)
+        db.session.commit()
 
     if request.method == 'POST':
         # get input values from inputform 
@@ -56,7 +70,7 @@ def round1():
         dem_cov = min(453, recieve_US + recieve_TW)
         service_level = min(100,round(((recieve_US + recieve_TW)/453)*100))
         revenue = min(recieve_US+recieve_TW, 453)*200
-        purchase = (order_US*100) + (order_TW*85)
+        purchase = (recieve_US*100) + (recieve_TW*85)
         holding = inventory * 15
         profit = revenue - purchase - holding
 
@@ -83,15 +97,79 @@ def round1():
 def round2():
 
     if request.method == 'POST':
-        return render_template("round2_feedback.html", content = "testing")
+
+        # get inventory and profit from database
+        query_profit = db.session.query(Data.profit).filter_by(round_id = 1).first()._asdict()
+        query_inventory = db.session.query(Data.inventory).filter_by(round_id = 1).first()._asdict()
+        profit_r1 = int(query_profit.get("profit"))
+        inventory_r1 = int(query_inventory.get("inventory"))
+        
+        # get data from form 
+        options = int(request.form.get('options') or 0)
+        analysis = int(request.form['analysis_purchase'] or 0)
+        order_US = int(request.form['order_US'] or 0)
+        order_TW = int(request.form['order_TW'] or 0)
+        order_CHN = 0 
+
+        # calculate whats happening
+        recieve_CHN = 0
+        recieve_US = order_US
+        if options == 13500: 
+            recieve_TW = round(order_TW*0.8)
+            compensation = 0 
+        elif options == 5000: 
+            recieve_TW = round(order_TW*0.4)
+            if inventory_r1 + recieve_US + recieve_TW < 370:
+                delta1 = 370 - inventory_r1 - recieve_US - recieve_TW
+                delta2 = order_TW - recieve_TW
+                compensation = (min(delta1, delta2))  * 57.5
+            else: 
+                compensation = 0
+        else: 
+            recieve_TW = round(order_TW*0.4)
+            compensation = 0  
+        inventory = max(0, inventory_r1 + recieve_US + recieve_TW - 370)
+        dem_cov = min(370, inventory_r1 + recieve_US + recieve_TW)
+        service_level = min(100,round(((dem_cov)/370)*100))
+        revenue = min(dem_cov, 370)*200
+        purchase = (recieve_US*100) + (recieve_TW*85)
+        holding = inventory * 15
+        profit = revenue + compensation - purchase - holding - analysis - options
+        total_profit = profit + profit_r1
+
+        # access Database row 2 and update Database
+        my_data = Data.query.get(2)
+        my_data.order_US = order_US
+        my_data.order_TW = order_TW
+        my_data.order_CHN = order_CHN
+        my_data.recieve_US = recieve_US
+        my_data.recieve_TW = recieve_TW
+        my_data.recieve_CHN = recieve_CHN
+        if analysis == 8000:
+            my_data.analysis = 1
+        else: 
+            None
+        if options == 13500:
+            my_data.measure1 = 1
+        elif options == 5000:
+            my_data.measure2 = 1
+        else:
+            None
+        my_data.inventory = inventory
+        my_data.profit = profit
+        db.session.commit()
+        
+        return render_template("round2_feedback.html", content = "testing", 
+        order_US=order_US, order_TW=order_TW, recieve_US=recieve_US, recieve_TW=recieve_TW, compensation=format(compensation, ",.2f"),
+        inventory=inventory, dem_cov=dem_cov, service_level=service_level, profit=format(profit, ",.2f"), total_profit=format(total_profit, ",.2f"))
     else:
         query_profit = db.session.query(Data.profit).filter_by(round_id = 1).first()._asdict()
         query_inventory = db.session.query(Data.inventory).filter_by(round_id = 1).first()._asdict()
-        profit = query_profit.get("profit")
-        inventory = query_inventory.get("inventory")
+        profit_r1 = query_profit.get("profit")
+        inventory_r1 = query_inventory.get("inventory")
         
         return render_template("round2.html", content = "testing", 
-        profit_r1 = profit, inventory_r1 = inventory)
+        profit_r1 = profit_r1, inventory_r1 = inventory_r1)
 
 
 @app.route('/round3/', methods=['GET', 'POST'])
